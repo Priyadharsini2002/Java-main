d194f55d018990d2ceb0c3db90b3aaa0974b20e1
Priyadharsini <110250602+Priyadharsini2002@users.noreply.github.com>
2024-03-10 19:11:17 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+public class add {
+   public static void main(String[] args) {
+
+    System.out.println(3+5);
+    System.out.println(8+7);
+
+    int num1 = 3;
+    int num2 = 3;
+    System.out.println(num1 + num2);
+}
+
+}
